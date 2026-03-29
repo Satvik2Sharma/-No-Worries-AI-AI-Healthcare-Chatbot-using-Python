b4f4024950735426cecb2a94acd5097c92b4fee1
Satvik2Sharma <satviksharma1706@gmail.com>
2026-03-29 18:49:11 +0530

    Delete healthcare-chatbot directory

--- healthcare-chatbot/test_entity-ectractor.py
+++
@@ -1,9 +0,0 @@
-from utils.entity_extractor import extract_symptoms, get_possible_conditions
-
-text = "I have headache and fever"
-
-symptoms = extract_symptoms(text)
-print("Detected:", symptoms)
-
-conditions = get_possible_conditions(symptoms)
-print("Possible conditions:", conditions)
--- healthcare-chatbot/test_nlp_pridiction.py
+++
@@ -1,4 +0,0 @@
-from utils.nlp import predict_intent
-
-print(predict_intent("I have headache"))
-print(predict_intent("hello bot"))
